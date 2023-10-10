d6826d2772bb7d4a549393a15b1495d1a24d4f68
sean3687 <129122794+th-sean@users.noreply.github.com>
2023-10-10 14:50:44 -0700

    loading edited

--- components/chatController.js
+++ components/chatController.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import Loading from "./animation/loading";
 import ScrollButton from "./scrollBottom";
 import { FaPaperPlane, FaTrashCan, FaRegComments } from "react-icons/fa6";
-import {PiBrainDuotone, PiArrowDown} from "react-icons/pi";
+import { PiBrainDuotone, PiArrowDown } from "react-icons/pi";
 import { icons } from "react-icons";
 import axios from "axios";
 
@@ -156,44 +156,44 @@ function ChatController({
                 ) : (
                   <div className="border-b bg-gray-50 ">
                     <div className="m-auto max-w-3xl p-5">
-                    <div className=" flex" key={key}>
-                      <div className="text-white">
-                        <PiBrainDuotone className="text-4xl fill-current bg-blue-600 rounded p-1" />
-                      </div>
-                      <div className="ml-5">
-                        {item.message}
-                        <div>
-                          <time className="text-xs opacity-50">
-                            {item.time}
-                          </time>
+                      <div className=" flex" key={key}>
+                        <div className="text-white">
+                          <PiBrainDuotone className="text-4xl fill-current bg-blue-600 rounded p-1" />
                         </div>
-                        {item.sender === "bot" &&
-                          item.fileData &&
-                          item.fileData.length > 0 && (
-                            <div className="flex text-xs items-center">
-                              <span className="text-sm font-bold mr-2">
-                                Learn more:
-                              </span>
-                              <div className="flex flex-wrap items-center">
-                                {" "}
-                                {item.fileData.map((data, index) => (
-                                  <button
-                                    key={index}
-                                    className="relative transform transition-transform px-2  mr-2 max-w-[130px] "
-                                    onClick={() => {
-                                      getDownloadDocument(data.file_id);
-                                    }}
-                                  >
-                                    <div className="relative group text-xs bg-cyan-500 px-2 py-1 rounded-lg text-white truncate max-w-[130px] hover:max-w-full">
-                                      {data.file_name}
-                                    </div>
-                                  </button>
-                                ))}
+                        <div className="ml-5">
+                          {item.message}
+                          <div>
+                            <time className="text-xs opacity-50">
+                              {item.time}
+                            </time>
+                          </div>
+                          {item.sender === "bot" &&
+                            item.fileData &&
+                            item.fileData.length > 0 && (
+                              <div className="flex text-xs items-center">
+                                <span className="text-sm font-bold mr-2">
+                                  Learn more:
+                                </span>
+                                <div className="flex flex-wrap items-center">
+                                  {" "}
+                                  {item.fileData.map((data, index) => (
+                                    <button
+                                      key={index}
+                                      className="relative transform transition-transform px-1 mr-1 max-w-[130px] "
+                                      onClick={() => {
+                                        getDownloadDocument(data.file_id);
+                                      }}
+                                    >
+                                      <div className="relative group text-xs bg-cyan-500 px-2 py-1 rounded-lg text-white truncate max-w-[130px] hover:max-w-full">
+                                        {data.file_name}
+                                      </div>
+                                    </button>
+                                  ))}
+                                </div>
                               </div>
-                            </div>
-                          )}
+                            )}
+                        </div>
                       </div>
-                    </div>
                     </div>
                   </div>
                 );
@@ -204,21 +204,23 @@ function ChatController({
         )}
         <div>
           {isSendChatLoading ? (
-            <>
-              <div className=" bg-gray-50 px-20 py-5 flex ">
-                <div></div>
+            <div className="border-b bg-gray-50">
+              <div className="m-auto max-w-3xl">
+              <div className=" bg-gray-50 p-5 flex ">
+              
                 <div className="text-white">
                   <PiBrainDuotone className="text-4xl fill-current bg-indigo-600 rounded p-1" />
                 </div>
                 <div className="chat-bubble chat-bubble-primary ml-5">
                   {streamingResponse}
-                  <div className="chat-bubble items-center chat-bubble-primary">
+                  <div className="chat-bubble items-center chat-bubble-primary mt-3">
                     <Loading />
                   </div>
                 </div>
               </div>
-              <div className="border-t border-gray-300"></div>
-            </>
+              
+              </div>
+            </div>
           ) : (
             <></>
           )}
@@ -232,74 +234,75 @@ function ChatController({
             "linear-gradient(rgba(255,255,255,0), rgba(220, 220, 220,1))",
         }}
       >
-        {messages.length !== 0 ?
-        (<button
-            className="relative mb-5 font-semibold shadow-sm rounded-full px-3 py-3 text-white ring-0 outline-none border-0 h-full opacity-75 bg-blue-600 mb-2 w-10 ml-auto mr-5" 
+        {messages.length !== 0 ? (
+          <button
+            className="relative mb-5 font-semibold shadow-sm rounded-full px-3 py-3 text-white ring-0 outline-none border-0 h-full opacity-75 bg-blue-600 mb-2 w-10 ml-auto mr-5"
             onClick={scrollToBottom}
-            
-        >
-            <PiArrowDown/>
-        </button>) : (<></>) 
-        }
-       
-       
+          >
+            <PiArrowDown />
+          </button>
+        ) : (
+          <></>
+        )}
+
         <div className="mx-4 mb-5 flex flex-col w-full @sm:pb-5 max-w-7xl m-auto">
           {/* Textarea/Input Box */}
           <div className="border rounded-lg">
-          <div className="">
-            <textarea
-              rows="4"
-              style={{ height: "45px", "overflow-y": "hidden" }}
-              className="block w-full text-gray-900 placeholder:text-gray-400 text-base font-normal resize-none outline-none px-4 py-4 rounded-t-lg focus:outline-none border-none bg-white z-5"
-              placeholder={
-                isSendChatLoading ? "Wait a second...." : "Type your message..."
-              }
-              value={inputText}
-              disabled={isSendChatLoading}
-              onChange={handleInputChange}
-              onKeyDown={handleEnter}
-            />
-          </div>
-          <div className="flex gap-2 justify-between p-2.5 bg-white border-b rounded-b-lg">
-            {/* Clear Button */}
-
-            <div className="flex-shrink-0 h-full px-2 py-1">
-              <button
-                className="transition-all duration-200 relative font-semibold shadow-sm rounded-md px-3 py-1.5 text-sm bg-blue-600 text-white ring-blue-600 active:ring-0 ring-0 hover:ring-0 outline-none hover:outline-none focus:outline-none border-0 h-full opacity-75"
-                onClick={handleRefresh}
-              >
-                <FaTrashCan className="text-xl mx-1" />
-              </button>
-            </div>
-            {/* Submit Button */}
-            <div className="flex-shrink-0 h-full px-2 py-1 flex">
-              <div className="flex items-center gap-2 mr-2">
-                <span className="ml-auto text-xs text-gray-500 transition-[color] duration-150 ease-in-out">
-                  {inputText.length}/6000
-                </span>
-              </div>
-              <button
-                className={
-                  "transition-all duration-200 relative font-semibold shadow-sm rounded-md px-3 py-1.5 text-sm text-white ring-blue-600 active:ring-0 ring-0 hover:ring-0 outline-none hover:outline-none focus:outline-none border-0 h-full opacity-75" +
-                  (isSendChatLoading
-                    ? " opacity-40 text-white "
-                    : " bg-blue-600 text-white")
+            <div className="">
+              <textarea
+                rows="4"
+                style={{ height: "45px", "overflow-y": "hidden" }}
+                className="block w-full text-gray-900 placeholder:text-gray-400 text-base font-normal resize-none outline-none px-4 py-4 rounded-t-lg focus:outline-none border-none bg-white z-5"
+                placeholder={
+                  isSendChatLoading
+                    ? "Wait a second...."
+                    : "Type your message..."
                 }
-                onClick={handleClick}
-              >
-                {isSendChatLoading ? (
-                  <Loading className="px-1 py-2" />
-                ) : (
-                  <FaPaperPlane className="text-xl" />
-                )}
-              </button>
+                value={inputText}
+                disabled={isSendChatLoading}
+                onChange={handleInputChange}
+                onKeyDown={handleEnter}
+              />
+            </div>
+            <div className="flex gap-2 justify-between p-2.5 bg-white border-b rounded-b-lg">
+              {/* Clear Button */}
+
+              <div className="flex-shrink-0 h-full px-2 py-1">
+                <button
+                  className="transition-all duration-200 relative font-semibold shadow-sm rounded-md px-3 py-1.5 text-sm bg-blue-600 text-white ring-blue-600 active:ring-0 ring-0 hover:ring-0 outline-none hover:outline-none focus:outline-none border-0 h-full opacity-75"
+                  onClick={handleRefresh}
+                >
+                  <FaTrashCan className="text-xl mx-1" />
+                </button>
+              </div>
+              {/* Submit Button */}
+              <div className="flex-shrink-0 h-full px-2 py-1 flex">
+                <div className="flex items-center gap-2 mr-2">
+                  <span className="ml-auto text-xs text-gray-500 transition-[color] duration-150 ease-in-out">
+                    {inputText.length}/6000
+                  </span>
+                </div>
+                <button
+                  className={
+                    "transition-all duration-200 relative font-semibold shadow-sm rounded-md px-3 py-1.5 text-sm text-white ring-blue-600 active:ring-0 ring-0 hover:ring-0 outline-none hover:outline-none focus:outline-none border-0 h-full opacity-75" +
+                    (isSendChatLoading
+                      ? " opacity-40 text-white "
+                      : " bg-blue-600 text-white")
+                  }
+                  onClick={handleClick}
+                >
+                  {isSendChatLoading ? (
+                    <Loading className="px-1 py-2" />
+                  ) : (
+                    <FaPaperPlane className="text-xl" />
+                  )}
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-       
       </div>
-      
+
       <div ref={messagesEndRef} />
     </div>
   );
