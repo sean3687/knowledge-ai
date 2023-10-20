5e355cd663fa2d544d5e1211dffefb045d9b0628
th-sean <sean.jin@teehub1.com>
2023-10-21 08:01:19 +0900

    username

---
+++ utils/usernameExtracter.js
@@ -0,0 +1,17 @@
+export default function extractUsername(inputString) {
+    // Check if the inputString is empty
+    if (inputString === "") {
+      return "";
+    }
+  
+    // Split the inputString by "@" delimiter
+    const parts = inputString.split("@");
+  
+    // If there is at least one part after splitting, return the first part (username)
+    if (parts.length > 0) {
+      return parts[0];
+    }
+  
+    // If no "@" is found, return the original string
+    return inputString;
+  }
